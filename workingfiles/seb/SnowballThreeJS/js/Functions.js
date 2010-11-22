9f856c29cbd40fd1ec9507e26d1ef26b80229a34
Seb Lee-Delisle <sebleedelisle@gmail.com>
2010-11-22 15:03:25 +0000

    now have three.js snowballs and planes

---
+++ workingfiles/seb/SnowballThreeJS/js/Functions.js
@@ -0,0 +1,76 @@
+var TO_RADIANS = Math.PI/180; 
+
+THREE.Vector3.prototype.rotateY = function(angle){
+					
+	cosRY = Math.cos(angle * TO_RADIANS);
+	sinRY = Math.sin(angle * TO_RADIANS);
+	
+	var tempz = this.z;; 
+	var tempx = this.x; 
+
+	this.x= (tempx*cosRY)+(tempz*sinRY);
+	this.z= (tempx*-sinRY)+(tempz*cosRY);
+
+
+}
+
+THREE.Vector3.prototype.rotateX = function(angle){
+					
+	cosRY = Math.cos(angle * TO_RADIANS);
+	sinRY = Math.sin(angle * TO_RADIANS);
+	
+	var tempz = this.z;; 
+	var tempy = this.y; 
+
+	this.y= (tempy*cosRY)+(tempz*sinRY);
+	this.z= (tempy*-sinRY)+(tempz*cosRY);
+
+
+}
+
+
+
+// Particle3D class
+
+Particle3D = function (material ) {
+
+	THREE.Particle.call( this );
+	
+	this.material = material instanceof Array ? material : [ material ];
+	
+	// define properties
+	this.velocity = new THREE.Vector3(0,0,0); 
+	this.gravity = new THREE.Vector3(0,0,0); 
+	this.drag = 1; 
+	// methods... 
+	
+};
+
+Particle3D.prototype = new THREE.Particle();
+Particle3D.prototype.constructor = Particle3D;
+
+Particle3D.prototype.update = function() {
+	
+	this.velocity.multiplyScalar(this.drag); 
+	this.velocity.addSelf(this.gravity);
+	this.position.addSelf(this.velocity);
+
+}
+
+
+// SnowBall class
+
+SnowBall = function () {
+	
+	Particle3D.call(this, new THREE.ParticleBitmapMaterial( particleImage )); 
+	
+	this.gravity.set(0,-1,0); 
+	this.drag = 0.99; 
+	
+	
+}
+
+SnowBall.prototype = new Particle3D();
+SnowBall.prototype.constructor = SnowBall;
+
+
--- workingfiles/seb/SnowballThreeJS/js/Particle3D.js
+++ workingfiles/seb/SnowballThreeJS/js/Particle3D.js
@@ -2,15 +2,3 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-SnowBall = function ( material ) {
-
-	THREE.Particle.call( this );
-
-	// define properties
-	this.xxxx = 0; 
-	// methods... 
-
-};
-
-SnowBall.prototype = new THREE.Particle();
-SnowBall.prototype.constructor = THREE.Particle;
